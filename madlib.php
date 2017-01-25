4bfe84bd0db8bb2b80d7beb0a0866b50e65379d5
Peter Verhaar <99021508@mydavinci.nl>
2017-01-25 11:08:18 +0100

    added input

---
+++ madlib.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Madlib story</title>
+	<link rel="stylesheet" type="text/css" href="madlib.css">
+</head>
+<body>
+	<main>
+		<section>
+		<h1>Welcome to the madlib story board</h1>
+		<h2>fill in all the inputs and klick "hit it" </h2>
+	
+			<form action='madlibget.php' method="get">
+			<p>any candy<input id="i2" type="text" name="candy"></p>
+			<p>an animal/object<input id="i1" type="text" name="object"></p>
+			<p>anything<input id="i3" type="text" name="anything"></p>
+			<p>music genre<input id="i4" type="text" name="music"></p>
+			<p>weapon*<input id="i5" type="text" name="weapon"></p>
+			<p>game<input id="i6" type="text" name="game"></p>
+			<p> melee weapon<input id="i7" type="text" name="melee"></p>
+			<p>* magic is a valid weapon FYI</p>
+			<input type="submit" name="HIT IT">
+			</form>
+		</section>
+	</main>
+</body>
+</html>
